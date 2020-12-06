0e204b6a90beec69048cc5e37ea8eac495e0e5b2
jimmynguyen <jimmy.nguyen@gatech.edu>
2020-12-05 20:07:26 -0500

    completed day 01 challenge

---
+++ christmas-challenges/2020/day01.py
@@ -0,0 +1,5 @@
+# It's that time of year again...
+# Challenge:
+# Write a program that asks a user for their name and outputs "Merry christmas, [name]"
+name = input("What is your name? ")
+print(f"Merry christmas, {name}")
